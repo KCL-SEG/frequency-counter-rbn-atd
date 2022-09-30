3fd573068a7fdf83ef7095c00fdd05aa5cf5e8b8
Reuben Atendido <K21048267@kcl.ac.uk>
2022-09-30 14:23:59 +0100

    fix error with int and str with same values e.g 4 and '4' not be grouped as the same value

--- frequencies.py
+++ frequencies.py
@@ -5,14 +5,12 @@ def frequencies(items):
     frequencies = {}
     
     for x in items:
-        if(frequencies.get(x) == None):
-            frequencies[x]=1
+        if(frequencies.get(str(x)) == None):
+            frequencies[str(x)]=1
         else:
-            frequencies[x]=frequencies.get(x)+1
+            frequencies[str(x)]=frequencies.get(str(x))+1
 
     return frequencies
 
-# print("enter values (strings) separated by commas: ")
-
-items =['0', 4,4,'4','d','d','e',0,'a','d','4']# thinglist = sorted([str(value) for value in input().split(",")])
+items =['0', 4,4,'4','d','d','e',0,'a','d','4']
 print(frequencies(items))
