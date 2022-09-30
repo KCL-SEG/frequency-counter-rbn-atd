e1becdc5e4791ab875e38a2b0653e1b51772f9cc
Reuben Atendido <K21048267@kcl.ac.uk>
2022-09-30 13:58:01 +0100

    attempt to extend code to sort frequency of list elements as a dictionary

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,14 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    print(items)
+    # for i in range(len(items)):
+    #     frequencies[items[i]]=items.count(items[i])
+    for x in items:
+        frequencies[x]=items.count(x)
+
     return frequencies
+
+print("enter values (strings) separated by commas: ")
+thinglist = sorted([str(value) for value in input().split(",")])
+print(frequencies(thinglist))
